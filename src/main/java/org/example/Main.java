cfec482afc78ad791bb995498f6fda6942d15f81
Nataliia Horb <nightangnata@gmail.com>
2022-09-13 00:26:05 +0200

    second commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,21 +2,40 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] mas = new int[]{1, 1, 2, 2, 4, 4, 7, 7, 15, 15, 26, 26, 27, 27, 38, 38, 39, 39, 50, 50, 51, 55, 55};
-        findElement(mas);
+
+
+        int[] mas1 = new int[]{1, 7, 23, 12, 40, 34, 7, 77, 3, 10};
+        findElement1(mas1, 7);
+
+
+        int[] mas2 = new int[]{1, 1, 2, 2, 4, 4, 7, 7, 15, 15, 26, 26, 27, 27, 38, 38, 39, 39, 50, 50, 51, 55, 55};
+        findElement2(mas2);
+    }
+
+    public static void findElement1(int mas[], int x) {
+        int count = 0;
+        for (int i = 0; i < mas.length; i++) {
+            if (x == mas[i]) {
+                System.out.println("This element is in array. Index number is " + i);
+                count++;
+            }
+        }
+        System.out.println("In total, there were " + count + " elements in the array");
+        System.out.println();
+
     }
 
     //space complexity=1+1=1
-    public static int findElement(int mas[]) {
-        for (int i = 0; i < mas.length - 1; i++) {
+    public static int findElement2(int mas[]) {
+        for (int i = 0; i < mas.length - 1; i += 2) {
             if (mas[i] == mas[i + 1]) {
-                i++;
             } else {
-                System.out.println(mas[i]);
+                System.out.println("A non-repeating element in an array is " + mas[i]);
                 return mas[i];
             }
         }
-        System.out.println(mas[mas.length - 1]);
+        System.out.println("A non-repeating element in an array is " + mas[mas.length - 1]);
         return mas[mas.length - 1];
     }
+
 }
